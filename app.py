9f09df453a106b570f2917b60962ae4091bd0da7
xxtyk <htolosarodriguez@gmail.com>
2026-03-19 04:50:31 +0000

    Ok

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
-from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    # Estas son las tarjetas que aparecerán en tu página
-    categorias = [
-        {'nombre': 'Cuidado del cabello', 'color': '#ffffff', 'icono': '🧴'},
-        {'nombre': 'Cocina', 'color': '#FF6F00', 'icono': '🍳'},
-        {'nombre': 'Mascotas', 'color': '#388E3C', 'icono': '🐾'},
-        {'nombre': 'Música y sonido', 'color': '#1E88E5', 'icono': '🎵'}
-    ]
-    # Si la carpeta templates/index.html no existe, aquí dará el error 500
-    return render_template('index.html', categorias=categorias)
-
 if __name__ == "__main__":
-    # Esto es OBLIGATORIO para que Render funcione
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
