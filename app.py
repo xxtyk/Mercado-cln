b649131cae037f7dcd26d5e60f366222200065dd
xxtyk <htolosarodriguez@gmail.com>
2026-03-19 04:57:52 +0000

    Ok

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# --- VENTANA 1: VISTA PARA CLIENTES ---
+@app.route('/')
+def index():
+    # Aquí jalaremos los productos de la base de datos después
+    return render_template('cliente.html')
+
+# --- VENTANA 2: CONFIGURACIÓN (ADMIN) ---
+@app.route('/admin')
+def admin():
+    # Aquí es donde tú gestionas todo
+    return render_template('admin.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
