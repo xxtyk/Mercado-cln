5791fdd8c2a85cf283fbf92f15059d8bb2be6d36
xxtyk <htolosarodriguez@gmail.com>
2026-03-21 18:06:03 -0700

    Update app.py

--- templates/app.py
+++ templates/app.py
@@ -1,1 +1,32 @@
+from flask import Flask, render_template, request, redirect
+import json
+import os
 
+app = Flask(__name__)
+
+DB = "productos.json"
+
+# crear archivo si no existe
+if not os.path.exists(DB):
+    with open(DB, "w") as f:
+        json.dump([], f)
+
+# funciones
+def cargar_productos():
+    with open(DB, "r") as f:
+        return json.load(f)
+
+def guardar_productos(data):
+    with open(DB, "w") as f:
+        json.dump(data, f, indent=4)
+
+# INICIO (TIENDA)
+@app.route('/')
+def inicio():
+    productos = cargar_productos()
+    return render_template('index.html', productos=productos)
+
+# AGREGAR PRODUCTO
+@app.route('/agregar', methods=['GET', 'POST'])
+def agregar():
+    if request.method
