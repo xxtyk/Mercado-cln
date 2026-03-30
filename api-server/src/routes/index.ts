6793d4620fff1276dabbbb1064aee127f201e573
xxtyk <htolosarodriguez@gmail.com>
2026-03-29 21:23:54 -0700

    Update index.ts

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -1,1 +1,18 @@
 
+import { Router } from "express";
+
+import health from "./health";
+import productos from "./productos";
+import categorias from "./categorias";
+import upload from "./upload";
+import webhook from "./webhook-proxy";
+
+const router = Router();
+
+router.use("/", health);
+router.use("/", productos);
+router.use("/", categorias);
+router.use("/", upload);
+router.use("/", webhook);
+
+export default router;
