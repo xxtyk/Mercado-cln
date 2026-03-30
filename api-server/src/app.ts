2dd95d0e3a31856c68671cbb6beb0823e6a6b14d
xxtyk <htolosarodriguez@gmail.com>
2026-03-29 22:16:41 -0700

    Update app.ts

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -1,1 +1,21 @@
 
+import express from "express";
+import cors from "cors";
+import path from "path";
+import router from "./routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+export default app;
