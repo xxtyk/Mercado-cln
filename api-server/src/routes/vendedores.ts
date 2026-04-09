6ed70d9cf68decbe5d0e0f6603e062fb11b5808b
xxtyk <htolosarodriguez@gmail.com>
2026-04-09 01:16:56 -0700

    Update vendedores.ts

--- api-server/src/routes/vendedores.ts
+++ api-server/src/routes/vendedores.ts
@@ -1,1 +1,25 @@
+import { Router } from "express";
 
+const router = Router();
+
+let vendedores = [];
+
+router.get("/vendedores", (_req, res) => {
+  res.json(vendedores);
+});
+
+router.post("/vendedores", (req, res) => {
+  vendedores.unshift({
+    id: Date.now().toString(),
+    nombre: req.body.nombre
+  });
+
+  res.json({ ok: true });
+});
+
+router.delete("/vendedores/:id", (req, res) => {
+  vendedores = vendedores.filter(v => v.id !== req.params.id);
+  res.json({ ok: true });
+});
+
+export default router;
