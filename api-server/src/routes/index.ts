e9e5c0303a5a189d37b572094ca6c02715197666
xxtyk <htolosarodriguez@gmail.com>
2026-04-09 01:02:52 -0700

    Update index.ts

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -6,6 +6,7 @@ import categorias from "./categorias.js";
 import upload from "./upload.js";
 import webhook from "./webhook-proxy.js";
 import pedidos from "./pedidos.js";
+import vendedores from "./vendedores.js";
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.use("/", categorias);
 router.use("/", upload);
 router.use("/", webhook);
 router.use("/", pedidos);
+router.use("/", vendedores);
 
 export default router;
