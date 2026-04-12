76ec7327457bf0a225ab52b0db26c1688eec425f
xxtyk <htolosarodriguez@gmail.com>
2026-04-12 14:13:52 -0700

    Update server.ts

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app.js";
+import { initDB } from "./initDB.js"; // 👈 NUEVO
 
 const PORT = process.env.PORT || 3000;
+
+// 👇 INICIALIZA BASE
+initDB();
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
